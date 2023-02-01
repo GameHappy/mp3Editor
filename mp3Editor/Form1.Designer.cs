ff8e60442176d261dfac6278a87ed0868d52195c
ffan1x <ffan1x@FFAN1X-MOBL>
2023-02-01 14:49:20 +0800

    test

--- mp3Editor/Form1.Designer.cs
+++ mp3Editor/Form1.Designer.cs
@@ -35,9 +35,10 @@ namespace mp3Editor
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(578, 282);
+            this.ClientSize = new System.Drawing.Size(308, 462);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
 
         }
